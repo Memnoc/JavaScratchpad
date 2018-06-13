eccac51e8008982fcad5254710610ef060d3a1b2
Memnoc <smartdroidesign@gmail.com>
2018-06-13 16:11:34 +0100

    update

--- src/com/smartdroid/ArrayListChallenge/Main.java
+++ src/com/smartdroid/ArrayListChallenge/Main.java
@@ -2,39 +2,81 @@ package com.smartdroid.ArrayListChallenge;
 
 // https://coderanch.com/t/271631/java/Multidimensional-ArrayList
 
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
+    private static Scanner scanner = new Scanner(System.in);
+    private static MobilePhone mobilePhone = new MobilePhone();
+
     public static void main(String[] args) {
+        boolean quit = false;
+        int choice = 0;
+        printMenu();
+        System.out.println("Enter your choice:");
+        choice = scanner.nextInt();
+        scanner.nextLine();
+        switch (choice) {
+            case 0:
+                printMenu();
+                break;
+            case 1: // print the list
+                mobilePhone.printList();
+                break;
+            case 2:
+                addContacts();
+                break;
+            case 3:
+                updateContacts();
+                break;
+            case 4:
+                removeContacts();
+                break;
+            case 5:
+                //searchContacts();
+                break;
+            case 6:
+                quit = true;
+                break;
 
-        ArrayList<String> names = new ArrayList<>();
-        names.add("Matteo");
-        names.add("Jim");
-        names.add("Mark");
-        names.add("Tom");
-        names.add("Mike");
-        ArrayList<String> numbers = new ArrayList<>();
-        numbers.add("12345");
-        numbers.add("3456");
-        numbers.add("111111");
-        numbers.add("22222");
-        numbers.add("444444");
+        }
+    }
 
-        ArrayList<ArrayList<String>> contacts = new ArrayList<ArrayList<String>>();
-        contacts.add(new ArrayList<String>());
-        (contacts.get(0)).addAll(names);
-        contacts.add(new ArrayList<>());
-        (contacts.get(1)).addAll(numbers);
-        int i = 0;
-        int j = 0;
-        // display contents of contacts
-        for (i = 0; i < contacts.size(); i++) {
-            for (j = 0; j < (contacts.get(i)).size(); j++) {
-                System.out.print((contacts.get(i)).get(j) + "  ");
-            }
-            System.out.println();
+        public static void printMenu() {
+            System.out.println("\nPress ");
+            System.out.println("\t 0 - To print choice options.");
+            System.out.println("\t 1 - To print the list of contacts.");
+            System.out.println("\t 2 - To add a contact to the list.");
+            System.out.println("\t 3 - To modify a contact in the list.");
+            System.out.println("\t 4 - To remove a contact from the list.");
+            System.out.println("\t 5 - To search for a contact in the list.");
+            System.out.println("\t 6 - To quit the application.");
         }
 
-    }
+        public static void addContacts() {
+            System.out.println("Please enter the contact name: ");
+            String name = scanner.nextLine();
+            System.out.println("Please enter phone number");
+            String number = scanner.nextLine();
+            Contact newContact = Contact.createContact(name, number);
+            mobilePhone.addContact(scanner.nextLine());
+        }
+
+        public static void updateContacts(){
+            System.out.println("Enter contact old name: ");
+            String name = scanner.nextLine();
+            System.out.println("Enter new name");
+            String newContact = scanner.nextLine();
+            mobilePhone.updateContact(newContact);
+        }
+
+        public static void removeContacts() {
+            System.out.println("Enter the contact to remove: ");
+            String name = scanner.nextLine();
+            mobilePhone.removeContact(name);
+        }
+
+
+
+
 }
--- src/com/smartdroid/ArrayListChallenge/MobilePhone.java
+++ src/com/smartdroid/ArrayListChallenge/MobilePhone.java
@@ -4,6 +4,87 @@ import java.util.ArrayList;
 
 public class MobilePhone {
 
-// code here
+    private ArrayList<Contact> contactList;
+    private String number;
 
+    public ArrayList<Contact> getContactList() {
+        return contactList;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String addContact(String name) {
+        addContact(name);
+        return name;
+    }
+
+
+    private boolean addContact(Contact contact) {
+        if (findContact(contact.getName()) >= 0) {
+            System.out.println("Contact is already on file");
+            return false;
+        }
+        contactList.add(contact);
+        return true;
+    }
+
+    public int findContact(String contactName) {
+        for (int i = 0; i < contactList.size(); i++) {
+            Contact contact = this.contactList.get(i);
+            if (contact.getName().equals(contactName)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private int findContact(Contact contact) {
+        return contactList.indexOf(contact);
+
+    }
+
+    public Contact removeContact(String name) {
+        int position = findContact(name);
+        if (position>=0){
+            return this.contactList.get(position);
+        }
+        return null;
+    }
+
+    public boolean removeContact(Contact contact) {
+        if (findContact(contact.getName()) >= 0) {
+            contactList.remove(contact);
+            return true;
+        }
+        System.out.println("Contact does not exists");
+        return false;
+    }
+
+    public Contact updateContact(String name) {
+        int position = findContact(name);
+        if (position>=0){
+            return this.contactList.get(position);
+        }
+        return null;
+    }
+
+    private boolean updateContact(Contact oldName, Contact newName) {
+        int position = findContact(oldName);
+        if (position<0){
+            System.out.println(oldName + " does not exists");
+            return false;
+        }
+        contactList.set(position, newName);
+        System.out.println("Contact at index " + position + oldName.getName() + " has been replaced by " + newName.getName());
+        return true;
+    }
+
+    public void printList() {
+        System.out.println("Contacts on phone: " + contactList.size());
+        for (int i = 0; i <contactList.size() ; i++) {
+            System.out.println((i+1) + ". " + contactList.get(i));
+        }
+    }
 }
